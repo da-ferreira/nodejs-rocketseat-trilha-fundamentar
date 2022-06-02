21a058385d5b73a6ac1e9986e32b5424cbbeffa9
David Ferreira <ferreiradavid361@gmail.com>
2022-06-02 16:48:55 -0300

    timeout

---
+++ 04 - Timers/setTimeout.js
@@ -0,0 +1,7 @@
+// SetTimeout vai rodar uma função depois de X milissegundos
+
+const timeOut = 3000; // 3 segundos
+const finished = () => console.log('done');
+
+setTimeout(finished, timeOut);
+console.log('Mostrar'); // Será executada antes
